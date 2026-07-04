e20c625f28edf2294237662452f35007407185c0
Shahed <khalaileh.shahed@gmail.com>
2026-07-04 16:53:22 +0300

    Add Product and Category entities with business rules

---
+++ SIOMS.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIOMS.Domain.Entities
+{
+    internal class Category
+    {
+    }
+}
---
+++ SIOMS.Domain/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIOMS.Domain.Entities
+{
+    internal class Product
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; } = null!;
+    }
+}
